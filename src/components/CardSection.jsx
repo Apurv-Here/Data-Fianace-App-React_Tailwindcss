2cc70376260e6681b1dc5e469f60c95a0edd230f
APURV CHOUBEY <apurv.main2@gmail.com>
2022-09-10 04:35:39 +0530

    added footer

--- src/components/CardSection.jsx
+++ src/components/CardSection.jsx
@@ -11,11 +11,11 @@ function CardSection() {
         <div className='w-full text-black shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 '>
           <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Double} alt="single" />
           <h2 className='text-2xl font-bold text-center py-8'>Double User</h2>
-          <p className='text-center text-4xl font-bold'>$149</p>
+          <p className='text-center text-4xl font-bold'>$299</p>
           <div className='text-center font-medium '>
-            <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-            <p className='py-2 border-b mx-8 '>1 Granted USer</p>
-            <p className='py-2 border-b mx-8 '>Send up to 2 GB</p>
+            <p className='py-2 border-b mx-8 mt-8'>1000 GB Storage</p>
+            <p className='py-2 border-b mx-8 '>2 Granted USer</p>
+            <p className='py-2 border-b mx-8 '>Send up to 4 GB</p>
           </div>
           <button className=' bg-[#00df9a] w-[160px] rounded-md font-bold mx-auto my-6 py-3  text-black hover:bg-green-200  '>Start Trail</button>
         </div>
@@ -39,11 +39,11 @@ function CardSection() {
         <div className='w-full text-black shadow-xl flex flex-col p-4 my-4 rounded-lg hover:scale-105 duration-300 '>
           <img className='w-20 mx-auto mt-[-3rem] bg-white' src={Triple} alt="single" />
           <h2 className='text-2xl font-bold text-center py-8'>Triple User</h2>
-          <p className='text-center text-4xl font-bold'>$149</p>
+          <p className='text-center text-4xl font-bold'>$399</p>
           <div className='text-center font-medium '>
-            <p className='py-2 border-b mx-8 mt-8'>500 GB Storage</p>
-            <p className='py-2 border-b mx-8 '>1 Granted USer</p>
-            <p className='py-2 border-b mx-8 '>Send up to 2 GB</p>
+            <p className='py-2 border-b mx-8 mt-8'>2000 GB Storage</p>
+            <p className='py-2 border-b mx-8 '>3 Granted USer</p>
+            <p className='py-2 border-b mx-8 '>Send up to 8 GB</p>
           </div>
           <button className=' bg-[#00df9a] w-[160px] rounded-md font-bold mx-auto my-6 py-3  text-black hover:bg-green-200  '>Start Trail</button>
         </div>
